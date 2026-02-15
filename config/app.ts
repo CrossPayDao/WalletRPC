f70a64f7a348488b3aa91795fae77ba17ff1ebe9
lucky <s@merypto.com>
2026-02-16 01:17:11 +0800

    chore(version): bump to 0.0.1 beta and add changelog

--- config/app.ts
+++ config/app.ts
@@ -2,4 +2,4 @@
 export const APP_NAME = "Wallet RPC";
 export const SEO_APP_NAME = "CPcash Wallet RPC";
 export const TAGLINE = "High-performance RPC Wallet (Beta)";
-export const APP_VERSION = "2.1.0-beta";
+export const APP_VERSION = "0.0.1";
